c0e78fb8ea072bc8e89170e1b1d1563d835f35b0
Simran Shah <simransah2401@gmail.com>
2022-06-02 14:14:30 +0530

    first commit

---
+++ src/formSource.js
@@ -0,0 +1,76 @@
+export const userInputs = [
+  {
+    id: 1,
+    label: 'Username',
+    type: 'text',
+    placeholder: 'username',
+  },
+  {
+    id: 2,
+    label: 'Name and surname',
+    type: 'text',
+    placeholder: 'name and surname',
+  },
+  {
+    id: 3,
+    label: 'Email',
+    type: 'mail',
+    placeholder: 'Enter your email',
+  },
+  {
+    id: 4,
+    label: 'Phone',
+    type: 'text',
+    placeholder: '+91 9876543210',
+  },
+  {
+    id: 5,
+    label: 'Password',
+    type: 'password',
+  },
+  {
+    id: 6,
+    label: 'Address',
+    type: 'text',
+    placeholder: 'Address',
+  },
+  {
+    id: 7,
+    label: 'Country',
+    type: 'text',
+    placeholder: 'Country',
+  },
+];
+
+export const productInputs = [
+  {
+    id: 1,
+    label: 'Title',
+    type: 'text',
+    placeholder: 'Apple Macbook Pro',
+  },
+  {
+    id: 2,
+    label: 'Description',
+    type: 'text',
+    placeholder: 'Description',
+  },
+  {
+    id: 3,
+    label: 'Category',
+    type: 'text',
+    placeholder: 'Computers',
+  },
+  {
+    id: 4,
+    label: 'Price',
+    type: 'text',
+    placeholder: '100',
+  },
+  {
+    id: 5,
+    label: 'Stock',
+    type: 'text',
+    placeholder: 'in stock',
+  },
+];
